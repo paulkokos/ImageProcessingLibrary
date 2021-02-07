71ff13b10dea693b2cd5349f7f4ba6e1754c28cd
paulkokos <paulkokos@yahoo.gr>
2021-02-07 02:58:29 +0200

    Initial Commit Dicom.cpp Dicom.hpp

---
+++ libs/Dicom.cpp
@@ -0,0 +1,8 @@
+//
+// Created by paulkokos on 7/2/21.
+//
+
+#include "Dicom.hpp"
+Dicom::Dicom() {
+
+}
---
+++ libs/Dicom.hpp
@@ -0,0 +1,16 @@
+//
+// Created by paulkokos on 7/2/21.
+//
+
+#ifndef IMAGEPROCESSINGLIBRARY_DICOM_HPP
+#define IMAGEPROCESSINGLIBRARY_DICOM_HPP
+
+
+class Dicom {
+public:
+    Dicom();
+
+};
+
+
+#endif //IMAGEPROCESSINGLIBRARY_DICOM_HPP
