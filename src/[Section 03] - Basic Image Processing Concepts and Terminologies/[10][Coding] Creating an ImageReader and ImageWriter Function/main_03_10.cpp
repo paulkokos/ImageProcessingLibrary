d0e86d7ef9fe90f94fee064fcaa2ff6d79dd6528
Pavlos Kokozidis <paulkokos@yahoo.gr>
2021-04-01 21:23:21 +0300

    Project Corrections

--- src/[Section 03] - Basic Image Processing Concepts and Terminologies/[10][Coding] Creating an ImageReader and ImageWriter Function/main_03_10.cpp
+++ src/[Section 03] - Basic Image Processing Concepts and Terminologies/[10][Coding] Creating an ImageReader and ImageWriter Function/main_03_10.cpp
@@ -1,59 +1,85 @@
 #include <iostream>
 
 
-#define BMP_HEADER_SIZE         g54
+#define BMP_HEADER_SIZE         54
 #define BMP_COLOR_TABLE_SIZE    1024
 #define CUSTOM_IMG_SIZE         1024*1024
+
+void imageWriter(const char *imageName,
+                 unsigned char *header,
+                 unsigned char* colorTable,
+                 unsigned char* buffer,
+                 int bitDepth);
+
+void imageReader(const char* imgName,
+                 int *height,
+                 int *width,
+                 int * bitDepth,
+                 unsigned char* header,
+                 unsigned char* colorTable,
+                 unsigned char* buffer);
 int main() {
-    FILE *streamIn = fopen("C:\\Users\\paulkokos\\CLionProjects\\"
-                           "ImageProcessingLibrary\\images\\input\\"
-                           "lena512.bmp","rb");
-    FILE *output = fopen("C:\\Users\\paulkokos\\CLionProjects\\"
-                         "ImageProcessingLibrary\\images\\output\\"
-                         "lena512_output.bmp","wb");
+    int imgWidth;
+    int imgHeight;
+    int imgBitDepth;
+    unsigned char imgHeader[BMP_HEADER_SIZE];
+    unsigned char imgColorTable[BMP_COLOR_TABLE_SIZE];
+    unsigned char imgBuffer[CUSTOM_IMG_SIZE];
 
-    if (streamIn == (FILE*)0) {
-        printf("Unable to open");
-    }
+    const char imgName[] = "C:\\Users\\paulkokos\\CLionProjects\\ImageProcessingLibrary\\images\\input\\kokozidis_0004.tif";
 
-    unsigned char header[54];
-    unsigned char colorTable[1024];
+    const char newImgName[] = "C:\\Users\\paulkokos\\CLionProjects\\ImageProcessingLibrary\\images\\output\\kokozidis_04_modified.tif";
 
-    for (int i =0; i < 54; i++) {
-        header[i]= getc(streamIn);
-    }
+    imageReader(imgName,&imgHeight,&imgWidth,&imgBitDepth,&imgHeader[0],&imgColorTable[0],&imgBuffer[0]);
 
-    int width = *(int *)&header[18];
-    int height = *(int *)&header[22];
-    int bitDepth = *(int *)&header[28];
+    imageWriter(newImgName,imgHeader,imgColorTable,imgBuffer,imgBitDepth);
 
-    if (bitDepth <=8) {
-        fread(colorTable,sizeof(unsigned char),1024,streamIn);
-    }
-    fwrite(header,sizeof (unsigned char),54,output);
-
-    //buffer
-
-    unsigned char buffer[height *width];
-    fread(buffer,sizeof (unsigned char),(height*width),streamIn);
-
-    if (bitDepth <=8) {
-        fwrite(colorTable,sizeof (unsigned char),1024,output);
-    }
-    fwrite(buffer,sizeof (unsigned char),(height*width),output);
-    fclose(output);
-    fclose(streamIn);
-    printf ("Success!\n");
-    printf ("Width is %d\n",width);
-    printf ("Height is %d\n",height);
+    printf("Success\n");
     return 0;
 }
 
-void imageHeader(const char* imgName, int *height, int *width, int * bitDepth, unsigned char* header, unsigned char* buffer) {
+void imageReader(const char* imgName,
+                 int *height,
+                 int *width,
+                 int * bitDepth,
+                 unsigned char* header,
+                 unsigned char* colorTable,
+                 unsigned char* buffer) {
     int i;
     FILE * streamIn;
     streamIn = fopen(imgName,"rb");
     if (streamIn == (FILE* )0) {
         printf("Unable to read image\n");
     }
+    for (int i=0; i<54; i++) {
+        header[i] = getc(streamIn);
+    }
+    *width = *(int *)&header[18];
+    *height = *(int* )&header[22];
+    *bitDepth = *(int* )&header[28];
+
+    if (*bitDepth <=8) {
+        fread(colorTable,sizeof (unsigned char), 1024, streamIn);
+    }
+
+    fread(buffer,sizeof (unsigned char),CUSTOM_IMG_SIZE,streamIn);
+
+    fclose(streamIn);
+}
+
+void imageWriter(const char *imageName,
+                 unsigned char *header,
+                 unsigned char* colorTable,
+                 unsigned char* buffer,
+                 int bitDepth) {
+
+    FILE *fileOutput = fopen(imageName,"wb");
+    fwrite(header,sizeof (unsigned char ),54,fileOutput);
+
+    if (bitDepth <=8) {
+        fwrite(colorTable,sizeof (unsigned char),1024,fileOutput);
+    }
+
+    fwrite(buffer,sizeof (unsigned char),CUSTOM_IMG_SIZE,fileOutput);
+    fclose(fileOutput);
 }
